e3d634ef39aec71b0e69fb9cf8a7f7991d34645c
macx <com@macx.de>
2011-06-08 13:56:36 +0200

    include timeout, rewrite catching methods, change facebook catch url

--- functions.php
+++ functions.php
@@ -1,7 +1,5 @@
 <?php
 
-add_action('the_post', 'socialDataGrabber');
-
 /**
  * socialDataGrabber
  * hook to get facebook and twitter social counts for each post
@@ -13,14 +11,17 @@ add_action('the_post', 'socialDataGrabber');
  * @return void
  */
 function socialDataGrabber($post) {
-	$permalinkUrl     = urlencode(get_permalink());
-	$transientApiKey  = 'post' . get_the_ID() . '_socialInfo';
-	$transientTimeout = (60 * 15);
-	$twitterAccount   = 'macx'; // set your twitter account name
-	$postTitleLimit   = 90;
+	$twitterAccount      = 'macx'; // set your twitter account name
+	$permalinkUrl        = get_permalink();
+	$permalinkUrlEncoded = urlencode($permalinkUrl);
+	$postTitle           = get_the_title();
+	$postTitle           = preg_replace('/(&#8211;|&#8212;)/i', '-', $postTitle);
+	$postTitleLimit      = 90;
+	$transientTimeout    = (60 * 15);
+	$transientApiKey     = 'post' . get_the_ID() . '_socialInfo';
 	
 	// debug
-	if($_GET['debugMode'] == 1) {
+	if($_GET['flushSocialGraph'] == 'flushAll') {
 		delete_transient($transientApiKey);
 	}
 	
@@ -31,40 +32,49 @@ function socialDataGrabber($post) {
 	} else {
 		$socialInfo = array();
 		
+		// set timeout
+		$context = stream_context_create(array('http' => array('timeout' => 3)));
+		
 		// get count data from twitter
-		$ch = curl_init();
-		curl_setopt_array($ch, array(
-			CURLOPT_URL            => 'http://urls.api.twitter.com/1/urls/count.json?url=' . $permalinkUrl,
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_FRESH_CONNECT  => 1,
-		));
-		$rawData = curl_exec($ch);
-		curl_close($ch);
+		$rawData = file_get_contents('http://urls.api.twitter.com/1/urls/count.json?url=' . $permalinkUrl, 0, $context);
 		
 		if($twitterData = json_decode($rawData)) {
-			$socialInfo['twitter']['count'] = $twitterData->count;
+			$socialInfo['twitter']['count'] = intval($twitterData->count);
+		} else {
+			$socialInfo['twitter']['count'] = intval(0);
 		}
 		
 		// get count data from facebook
-		// see: http://developers.facebook.com/docs/reference/rest/links.getStats
+		// see: https://developers.facebook.com/docs/reference/api/
+		$rawData = file_get_contents('http://graph.facebook.com/?ids=' . $permalinkUrl, 0, $context);
+		
+		if($facebookData = json_decode($rawData, true)) {
+			$socialInfo['facebook']['count'] = intval($facebookData[$permalinkUrl]['shares']);
+		} else {
+			$socialInfo['facebook']['count'] = intval(0);
+		}
+		
+		// setup google +1 button
+		// see: http://code.google.com/intl/de-AT/apis/+1button/
 		$ch = curl_init();
 		curl_setopt_array($ch, array(
-			CURLOPT_URL            => 'http://api.facebook.com/method/links.getStats?urls=' . $permalinkUrl . '&format=json',
-			CURLOPT_RETURNTRANSFER => 1,
-			CURLOPT_FRESH_CONNECT  => 1,
+			CURLOPT_URL            => 'https://clients6.google.com/rpc',
+			CURLOPT_POST           => 1,
+			CURLOPT_POSTFIELDS     => '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $permalinkUrl . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]',
+			CURLOPT_RETURNTRANSFER => true,
+			CURLOPT_HTTPHEADER     => array('Content-type: application/json'),
+			CURLOPT_RETURNTRANSFER => 3,
+			CURLOPT_TIMEOUT        => 3,
+			CURLOPT_CONNECTTIMEOUT => 3
 		));
 		$rawData = curl_exec($ch);
 		curl_close($ch);
 		
-		if($facebookData = json_decode($rawData)) {
-			$socialInfo['facebook']['count'] = $facebookData[0]->total_count;
+		if($plusOneData = json_decode($rawData, true)) {
+			$socialInfo['plusone']['count'] = intval($plusOneData[0]['result']['metadata']['globalCounts']['count']);
+		} else {
+			$socialInfo['plusone']['count'] = intval(0);
 		}
-		
-		// setup title and links
-		$postTitle      = get_the_title();
-		$postTitle      = preg_replace('/(&#8211;|&#8212;)/i', '-', $postTitle);
-		$postUrl        = get_permalink();
-		$postUrlEncoded = ($postUrl);
 		
 		// setup twitter
 		if(strlen($postTitle) > $postTitleLimit) {
@@ -74,32 +84,14 @@ function socialDataGrabber($post) {
 		}
 		
 		$message                            = urlencode($twitterPostTitle);
-		#$socialInfo['twitter']['shareUrl']  = 'http://twitter.com/share?text=' .$message . '&url=' . $postUrlEncoded . '&related=' . $twitterAccount . '&via=' . $twitterAccount;
-		$socialInfo['twitter']['shareUrl']  = 'http://twitter.com/intent/tweet?related=' . $twitterAccount . '&text=' .$message . '&url=' . $postUrlEncoded . '&via=' . $twitterAccount . '&lang=de';
+		$socialInfo['twitter']['shareUrl']  = 'http://twitter.com/intent/tweet?related=' . $twitterAccount . '&text=' .$message . '&url=' . $permalinkUrl . '&via=' . $twitterAccount . '&lang=de';
 		
 		// setup facebook
-		$socialInfo['facebook']['shareUrl'] = 'http://www.facebook.com/sharer.php?u=' . $postUrlEncoded . '&t=' . urlencode($postTitle);
+		$socialInfo['facebook']['shareUrl'] = 'http://www.facebook.com/sharer.php?u=' . $permalinkUrl . '&t=' . urlencode($postTitle);
 		
 		// setup google +1 button
-		// see: http://code.google.com/intl/de-AT/apis/+1button/
-		$socialInfo['plusone']['shareUrl'] = 'https://plusone.google.com/u/0/+1/profile/?type=po&ru=' . $postUrlEncoded;
+		$socialInfo['plusone']['shareUrl']  = 'https://plusone.google.com/u/0/+1/profile/?type=po&ru=' . $permalinkUrl;
 		
-		$ch = curl_init();
-		curl_setopt_array($ch, array(
-			CURLOPT_URL            => 'https://clients6.google.com/rpc?key=AIzaSyCKSbrvQasunBoV16zDH9R33D88CeLr9gQ',
-			CURLOPT_POST           => 1,
-			CURLOPT_POSTFIELDS     => '[{"method":"pos.plusones.get","id":"p","params":{"nolog":true,"id":"' . $postUrl . '","source":"widget","userId":"@viewer","groupId":"@self"},"jsonrpc":"2.0","key":"p","apiVersion":"v1"}]',
-			CURLOPT_RETURNTRANSFER => true,
-			CURLOPT_HTTPHEADER     => array('Content-type: application/json'),
-			CURLOPT_FRESH_CONNECT  => 1,
-		));
-		$rawData = curl_exec($ch);
-		curl_close($ch);
-		
-		if($plusOneData = json_decode($rawData, true)) {
-			$socialInfo['plusone']['count'] = $plusOneData[0]['result']['metadata']['globalCounts']['count'];
-		}
-
 		// attach results to $post object
 		$post->socialInfo = $socialInfo;
 		
@@ -107,3 +99,5 @@ function socialDataGrabber($post) {
 		set_transient($transientApiKey, $socialInfo, $transientTimeout);
 	}
 }
+
+add_action('the_post', 'socialDataGrabber');
